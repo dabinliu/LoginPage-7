f41ed38787d66c623d0a949bfe949c6487011cb0
Thorin <c.nethercott@icloud.com>
2016-06-05 16:05:24 +0100

    Done some stuff

--- js/chatout.login.js
+++ js/chatout.login.js
@@ -7,9 +7,15 @@ $(document).ready(function(){
 		$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 	});
 	$('#register').click(function() {
-		alert("You registered");
+		var rusername = $('#rusername').val();
+		var rpassword = $('#rpassword').val();
+		var remail = $('#remail').val();
+		var rinvite = $('#rinvite').val();
+		alert(rusername + " " + rpassword + " " + remail + " " + rinvite);
 	});
 	$('#login').click(function() {
-		alert("You logged in");
+		var eusername = $('#eusername').val();
+		var epassword = $('#epassword').val();
+		alert(eusername + " " + epassword);
 	});
 });
