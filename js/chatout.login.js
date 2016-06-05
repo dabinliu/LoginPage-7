7ff832bfbb894f83cb8ae2f0be6ac3757c21f971
Thorin <c.nethercott@icloud.com>
2016-06-05 14:20:10 +0100

    Added some proof of concept stuff.

--- js/chatout.login.js
+++ js/chatout.login.js
@@ -1,7 +1,15 @@
 /*
 	Chatout
 	chatout.login.js
-*/
+*/ 
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$('#register').click(function() {
+	alert("You registered");
+});
+
+$('#login').click(function() {
+	alert("You logged in");
 });
